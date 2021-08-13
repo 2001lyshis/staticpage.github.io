efa12ec0bb2164e099e73d50e1757858788b1aee
lyshis <lyshis@163.com>
2021-08-13 14:35:01 +0800

    index bug fix9

--- index/js/banner.js
+++ index/js/banner.js
@@ -1,7 +1,7 @@
 let init=()=>{
-                layer[0].children[0].style='transition:.2s linear;object-fit: cover;transform: scale(1) translate(0px) ; filter: blur(0px);opacity: 0;';
-                layer[1].children[0].style='transition:.2s linear;object-fit: cover;transform: scale(1) translate(0px) ; filter: blur(0px);opacity: 1;';
-                layer[2].children[0].style='transition:.2s linear;object-fit: cover;transform: scale(1) translate(0px) ; filter: blur(0px);opacity: 0;';
+                layer[0].children[0].style='transition:.2s linear;object-fit: cover;transform:  translate(0px) ;opacity: 0;';
+                layer[1].children[0].style='transition:.2s linear;object-fit: cover;transform:  translate(0px) ;opacity: 1;';
+                layer[2].children[0].style='transition:.2s linear;object-fit: cover;transform:  translate(0px) ;opacity: 0;';
             };
             var banner = document.getElementById("banner1");
             let layer=document.querySelectorAll(".layer");
@@ -14,23 +14,23 @@ let init=()=>{
                 {scale:1,translate:0,filter:0,opacity:0},
                 {scale:1,translate:0,filter:0,opacity:1},
                 {scale:1,translate:0,filter:0,opacity:0},
-                {scale:1,translate:0,filter:0,opacity:0},
             ];
             banner.ontouchstart = function(e){
                 e.preventDefault();
                 x=e.touches[0].clientX;
             }
             banner.ontouchmove = function(e){
-                    x_new =e.touches[0].clientX;
-                    x_offset=x-x_new;
-                    const element_data1=data_images[1];
-                    const element_data2=data_images[2];
-                    let opacity_new1=element_data1.opacity+(x_offset/step);
-                    let opacity_new2=element_data2.opacity+(x_offset/step);
-                    let translate_new=-x_offset / 2;
-                    layer[0].children[0].style='transform: scale(1) translate('+translate_new+'px) ; filter: blur(0px);opacity:1;';
-                    layer[1].children[0].style='transform: scale(1) translate('+translate_new+'px) ; filter: blur(0px);opacity:'+opacity_new1+';';
-                    layer[2].children[0].style='transform: scale(1) translate('+translate_new+'px) ; filter: blur(0px);opacity:'+opacity_new2+';';
+                e.preventDefault();
+                x_new =e.touches[0].clientX;
+                x_offset=x-x_new;
+                const element_data1=data_images[1];
+                const element_data2=data_images[2];
+                let opacity_new1=element_data1.opacity+(x_offset/step);
+                let opacity_new2=element_data2.opacity+(x_offset/step);
+                let translate_new=-x_offset / 2;
+                layer[0].children[0].style='transform: translate('+translate_new+'px) ; opacity:1;';
+                layer[1].children[0].style='transform: translate('+translate_new+'px) ; opacity:'+opacity_new1+';';
+                layer[2].children[0].style='transform: translate('+translate_new+'px) ; opacity:'+opacity_new2+';';
 
             }
             banner.ontouchend= function() {
@@ -46,10 +46,9 @@ let init=()=>{
                 const element_data2=data_images[2];
                 let opacity_new1=element_data1.opacity+(x_offset/step);
                 let opacity_new2=element_data2.opacity+(x_offset/step);
-                let translate_new=-x_offset/30;
-                layer[0].children[0].style='transform: scale(1) translate('+translate_new+'px) ; filter: blur(0px);opacity:1;';
-                layer[1].children[0].style='transform: scale(1) translate('+translate_new+'px) ; filter: blur(0px);opacity:'+opacity_new1+';';
-                layer[2].children[0].style='transform: scale(1) translate('+translate_new+'px) ; filter: blur(0px);opacity:'+opacity_new2+';';
+                layer[0].children[0].style='filter: blur(0px);opacity:1;';
+                layer[1].children[0].style='filter: blur(0px);opacity:'+opacity_new1+';';
+                layer[2].children[0].style='filter: blur(0px);opacity:'+opacity_new2+';';
             }
             banner.onmouseout = function(e){
                 init();
