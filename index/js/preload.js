3cb1c127ef9569364eb0756560b1ee61a7705cbe
lyshis <lyshis@163.com>
2021-09-22 22:47:06 +0800

    9-22

--- index/js/preload.js
+++ index/js/preload.js
@@ -5,7 +5,7 @@ var _PageHeight = document.documentElement.clientHeight,
 var _LoadingTop = _PageHeight > 61 ? (_PageHeight - 611) / 2 : 0,
     _LoadingLeft = _PageWidth > 215 ? (_PageWidth - 715) / 2 : 0;
 //在页面未加载完毕之前显示的loading Html自定义内容
-var _LoadingHtml = '<div id="loadingDiv" style="position:absolute;left:0;width:100%;height:' + _PageHeight + 'px;top:0;background:#f3f8ff;opacity:1;filter:alpha(opacity=80);z-index:10000;"><div style="position: absolute; cursor1: wait; left: ' + _LoadingLeft + 'px; top:' + _LoadingTop + 'px; width: auto; height: 527px; line-height: 527px; padding-left: 500px; padding-right: 50px; background: #fff url(index/img/Reimu.gif) no-repeat scroll 5px 10px; border: 2px solid #95B8E7; font-family:\'Microsoft YaHei\';">页面加载中，请等待...</div></div>';
+var _LoadingHtml = '<div id="loadingDiv" style="position:absolute;left:0;width:100%;height:' + _PageHeight + 'px;top:0;background:#f3f8ff;opacity:1;filter:alpha(opacity=80);z-index:10000;"><div style="position: absolute; cursor1: wait; left: ' + _LoadingLeft + 'px; top:' + _LoadingTop + 'px; width: auto; height: 527px; line-height: 527px; padding-left: 500px; padding-right: 50px; background: #fff url(index/img/Reimu.gif) no-repeat scroll 5px 10px; border: 2px solid #95B8E7; font-family:\'Microsoft YaHei\';">页面加载中，稍等...</div></div>';
 //呈现loading效果
 document.write(_LoadingHtml);
 
